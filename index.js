e70031e49ef3f6721a6096b46fddfc3efaa93225
Robert Masen <r.f.masen@gmail.com>
2016-11-03 16:27:39 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+(function() {
+    var req = new XMLHttpRequest()
+    req.onreadystatechange = function() {
+        if (req.readyState == 4 && this.status == 200) {
+            let json = JSON.parse(this.responseText)
+            let main = document.getElementById('main')
+            for (i=0;i<json.length;i++) {
+                var element = json[i]
+                main.innerHTML += '<li><h3>'+element[name]'</h3><br><span>'+element[description]'</span></li>'
+             }
+         }
+     }
+     req.open('get', 'http://robertmasen.com/github', true)
+     req.send()
+})()
